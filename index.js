19308b910904dcc741f7c3bb42b14f2b4d0658d9
Boniface Musyoki <bonifacemusyoki@gmail.com>
2025-01-28 09:00:25 -0800

    updated changes

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ try {
     const jsNum1 = core.getInput('number1');
     const jsNum2 = core.getInput('number2');
     result = parseInt(jsNum1) + parseInt(jsNum2);
-    core.setOutput('result'.result);
+    core.setOutput("result", result);
 } catch(error){
     core.setFailed(error.message)
 }
